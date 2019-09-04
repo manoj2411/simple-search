384ca21a7700e4923ea1b8218053e06598ed6fa4
Manoj <manoj.delhi24@gmail.com>
2019-09-04 07:56:25 +0700

    created inverted index. Search word from given index

--- src/main/scala/test/SimpleSearch.scala
+++ src/main/scala/test/SimpleSearch.scala
@@ -18,6 +18,8 @@ object Program {
 
   import scala.io.StdIn.readLine
 
+  type InvertedIndex = Map[String, Set[File]]
+
   sealed trait ReadFileError
 
   case object MissingPathArg extends ReadFileError
@@ -25,18 +27,41 @@ object Program {
   case class FileNotFound(t: Throwable) extends ReadFileError
 
   // TODO: Implement this
-  case class Index()
+  case class Index(invertedIndex: InvertedIndex = Map.empty)
+
+  class Parser {
+
+    val WORD = "\\w+".r
+
+    def parseToWords(text: String) =
+      WORD.findAllIn(text).map(_.toLowerCase)
+  }
+
+  object Parser {
+    def apply() = new Parser
+  }
 
   // TODO: Index all files in the directory
   def index(file: File): Index = {
-    println("Generating index...")
+    // println("Generating index...")
+
     val files = file.listFiles
+    var invertedIndex : InvertedIndex = Map.empty
+
     files.foreach { innerFile =>
       val source = scala.io.Source.fromFile(innerFile.getAbsolutePath, "ISO-8859-1") // handle encoding errors
-      val lines = try source.mkString finally source.close()
-      println(innerFile.getName + "\t" + lines.length)
+      try {
+        source.getLines.flatMap { line =>
+          Parser().parseToWords(line)
+        }.foreach { word =>
+          val newFileList = invertedIndex.getOrElse(word, Set()) + innerFile
+          invertedIndex += (word -> newFileList)
+        }
+      }
+      finally source.close()
     }
-    Index()
+    println(s"${files.length} files read in directory ${ file.getAbsolutePath }")
+    Index(invertedIndex)
   }
 
   def readFile(args: Array[String]): Either[ReadFileError, File] = {
@@ -58,6 +83,12 @@ object Program {
     val searchString = readLine()
     if (searchString != ":quit") {
       // TODO: Make it print the ranking of each file and its corresponding score
+      Parser().parseToWords(searchString).foreach { word =>
+        val files = indexedFiles.invertedIndex.get(word).map { files =>
+          val names = files.map(_.getName).toList.take(10)
+          println(s"$word : ${ names.mkString(", ") }")
+        }
+      }
       iterate(indexedFiles)
     }
   }
